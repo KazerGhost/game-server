df6ef591bc6bfad9ec97fa21a8f8ea08db21da47
Kasper <kaserplayz@gmail.com>
2026-01-26 12:31:11 -0500

    swagger fix 3

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import bodyParser from 'body-parser';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
+// controllers
 import games from './controllers/games';
 
 const app: Application = express();
 
+// configure app globally to parse http request bodies as json
 app.use(bodyParser.json());
 
+// url dispatching
 app.use('/api/v1/games', games);
 
 // swagger api doc config
@@ -24,16 +27,19 @@ const options = {
 }
 
 const openApiSpecs = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUi.serve);
 
+// hard-code swagger css & js links using public Content Delivery Network (CDN)
 app.get('/api-docs', (req: Request, res: Response) => {
     const html: string = swaggerUi.generateHTML(openApiSpecs, {
         customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
         customJs: [
-         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
         ]
-      });
+    });
 
     res.send(html);
 });
+
 app.listen(4000, () => { console.log('Server running on port 4000') });
